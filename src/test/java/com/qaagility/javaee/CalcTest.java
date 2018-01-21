e89837569ea4f25989aff3d823f5f76ded1cf668
alokagarwal10 <alokagarwal10@gmail.com>
2018-01-21 15:30:37 +0530

    Update CalcTest.java

--- src/test/java/com/qaagility/javaee/CalcTest.java
+++ src/test/java/com/qaagility/javaee/CalcTest.java
@@ -2,7 +2,7 @@ package com.qaagility.javaee;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
- 
+import com.qaagility.javaee.Calculator; 
 public class CalcTest {
 
       public void testSlow() {
